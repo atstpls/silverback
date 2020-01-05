902bec84bd2b6bd09d2f00cf1b9b3b4be7b3f708
atstpls <25083331+atstpls@users.noreply.github.com>
2020-01-05 11:25:48 -0600

    Update getOffensiveTools.sh

--- getOffensiveTools.sh
+++ getOffensiveTools.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
+verify_command(){
+if ! command -v $1 > /dev/null 
+then
+	echo "- $1 not installed"
+	exit
+else 
+  echo " + $1 installed" 
+fi
+}
+
+verify_file(){
+if ! file $1 > /dev/null 
+then
+	echo "- $1 not installed"
+	exit
+else 
+  echo " + $1 installed" 
+fi
+}
+
+
 # enumeration 
-echo "> Getting Gobuster.."
 apt install gobuster -y
-if (command -v gobuster 1>/dev/null)
-then echo "+ Gobuster installed at $(which gobuster)"
+verify Gobuster
 
 # privesc
-echo "> Getting windows-privesc-check2.exe..."
 wget https://github.com/pentestmonkey/windows-privesc-check/raw/master/windows-privesc-check2.exe -O wpc2.exe
-if (file wpc2.exe 1>/dev/null)
-then echo "+ windows-privesc-check2 downloaded as wpc2.exe"
+verify_file wpc2.exe
 
-echo "> Getting PowerUp.ps1..." 
 wget https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/privesc/PowerUp.ps1
-if (file PowerUp.ps1 1>/dev/null)
-then echo "+ PowerUp.ps1 downloaded"
+verify_file PowerUp.ps1
 
+wget https://raw.githubusercontent.com/sleventyeleven/linuxprivchecker/master/linuxprivchecker.py
+verify_file linuxprivchecker.py
 
-# linuxprivchecker.py
-# Sherlock.ps1 
-# windows-exploit-suggester.py 
+wget https://raw.githubusercontent.com/rasta-mouse/Sherlock/master/Sherlock.ps1
+verify_file Sherlock.ps1
+
+wget https://github.com/AonCyberLabs/Windows-Exploit-Suggester/blob/master/windows-exploit-suggester.py
+verify_file windows-exploit-suggester.py
 
 # shells
-# nc.exe 
-# webshell.aspx  
-# wshell.war
