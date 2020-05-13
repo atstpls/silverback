d66a27d080ec8506737150bbe7afddd23dd4d733
atstpls <25083331+atstpls@users.noreply.github.com>
2020-05-13 10:09:53 -0500

    Update func.sh

--- func.sh
+++ func.sh
@@ -49,7 +49,7 @@ getResponses() {
   for x in $(cat $1)
   do
     NAME=$(echo $x | awk -F/ '{print $3}')
-    curl -X GET -H "X-Forwarded-For: evil.com" $x -I > "$CURRENT_PATH/headers/$NAME"
-    curl -s -X GET -H "X-Forwarded-For: evil.com" -L $x > "$CURRENT_PATH/responsebody/$NAME"
+    curl -sk -X GET -H "X-Forwarded-For: evil.com" $x -I > "$CURRENT_PATH/headers/$NAME"
+    curl -sk -X GET -H "X-Forwarded-For: evil.com" -L $x > "$CURRENT_PATH/responsebody/$NAME"
   done
 }
