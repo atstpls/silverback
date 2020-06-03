5b3344597675f4257fb7601309b1ec258198e1e2
atstpls <25083331+atstpls@users.noreply.github.com>
2020-06-03 06:52:53 -0500

    Update func.sh

--- func.sh
+++ func.sh
@@ -5,7 +5,6 @@ alias scpp='scp -o PreferredAuthentications=password -o PubkeyAuthentication=no 
 alias python="python3"
 
 alias urldecode='python3 -c "import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))"'
-
 alias urlencode='python3 -c "import sys, urllib.parse as ul; print (ul.quote_plus(sys.argv[1]))"'
 
 hgrep () {
@@ -14,11 +13,6 @@ hgrep () {
 
 getCert () {
     timeout 3 openssl s_client -showcerts -servername $1 -connect $1:443 <<< "Q" 2> /dev/null | openssl x509 -text -noout | grep DNS | tr ',' '\n' | cut -d ':' -f 2 | sort -fu
-}
-
-FindHosts() {
-  subfinder -d $1 >> Hosts1 | assetfinder -subs-only $1 >> Hosts1 | amass enum -norecursive -noalts -d $1 >> Hosts1 
-  awk '!a[$0]++' Hosts1 > Hosts
 }
 
 FindTakeovers() {
@@ -52,4 +46,22 @@ getResponses() {
     curl -sk -X GET -H "X-Forwarded-For: evil.com" $x -I > "$CURRENT_PATH/headers/$NAME"
     curl -sk -X GET -H "X-Forwarded-For: evil.com" -L $x > "$CURRENT_PATH/responsebody/$NAME"
   done
+}
+
+fix() {
+  /etc/init.d/network-manager restart
+  echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf
+  echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+}
+
+export PATH=$PATH:~/go/bin:~/scripts/Python
+
+RemoveDockerNones() {
+  docker images | grep none | awk '{ print $3; }' | xargs docker rmi -f
+}
+
+createVenv() {
+   python3 -m venv venv
+   source venv/bin/activate
+   echo $(pwd) > $(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")/path.pth
 }
